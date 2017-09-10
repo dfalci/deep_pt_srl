8be8bc5dd08877d6ebe18c72423f19f5fa4d42ad
danielfalci <danielfalci@gmail.com>
2017-09-10 15:52:38 -0300

    importing modules

--- utils/config.py
+++ utils/config.py
@@ -28,7 +28,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from singleton import Singleton
+from utils import Singleton
 import json
 
 @Singleton
