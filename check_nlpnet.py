dbe602314c95b912d805e27574631d3e954da2b8
danielfalci <danielfalci@gmail.com>
2017-11-27 21:37:03 -0200

    semantic role labeler for the portuguese language

--- check_nlpnet.py
+++ check_nlpnet.py
@@ -35,7 +35,7 @@ def handleTag(tag):
     else:
         return [u'*']
 
-def tagSentence(tagger, predicate, sentence):
+def tagSentence(tagger, predicate, sentence, verbPosition):
     print sentence, predicate
     temporary = sentence
     quant = 0
@@ -54,24 +54,70 @@ def tagSentence(tagger, predicate, sentence):
     finalTags = []
     for item in temporary:
         finalTags.extend(handleTag(item))
+    if len(mapa) == 0 and verbPosition != -1:
+        finalTags[verbPosition] = 'u(V*V)'
     return finalTags, tokens
 
 
-def tagGoldSentence(predicate, goldRoles):
+def tagGoldSentence(predicate, sentence, goldRoles):
     roles = goldRoles.split(' ')
+    verbPosition = -1
+    retorno = []
+    for i in xrange(0, len(roles)):
+        item = {'predicate':'-', 'end':'', 'start':''}
+        currentTag = roles[i]
+        nextTag = None
+        if i < len(roles) - 1:
+            nextTag = roles[i+1]
+
+        if currentTag == 'O':
+            item['start'] = u'*'
+
+
+        if currentTag.startswith('B-'):
+            item['start'] = '('+currentTag[2:]+u'*'
+            if nextTag == None or not nextTag.startswith('I-'):
+                item['end'] = currentTag[2:]+u')'
+
+        if currentTag.startswith('I-'):
+            item['start'] = u'*'
+            if nextTag == None or nextTag != currentTag:
+                item['end'] = currentTag[2:]+u')'
+
+        if currentTag == 'V':
+            item['start'] = u'(V*'
+            item['end'] = u'V)'
+            item['predicate'] = u'-'
+            #verbPosition = i
+        retorno.append(item['start'] + item['end'])
+    return retorno, verbPosition
 
 
 
 file = open('./results/erick/testenlp.props', "w")
-#goldFile = open('./results/erick/gold.props', "w")
+goldFile = open('./results/erick/gold.props', "w")
 data = pd.read_csv('./resources/corpus/converted/propbank_test.csv', encoding='utf-8')
 iterations = len(data)
 for i in xrange(0, iterations):
     sentence = data['sentence'][i]
     predicate = data['predicate'][i]
     goldRoles = data['roles'][i]
-    sent, tokens = tagSentence(tagger, predicate, sentence)
-    for x, t in enumerate(sent, tokens):
-        file.write('{0}\t{1}\n'.format(t, x))
+    gold, verbPosition = tagGoldSentence(predicate, sentence, goldRoles)
+    nlptagged, tokens = tagSentence(tagger, predicate, sentence, verbPosition)
+
+    assert(len(nlptagged) == len(gold))
+
+    for x in nlptagged:
+        if x == u'(V*V)':
+            file.write('{0}\t{1}\n'.format( str(i), x))
+        else:
+            file.write('{0}\t{1}\n'.format( u'-', x))
+    for x in gold:
+        if x == u'(V*V)':
+            goldFile.write('{0}\t{1}\n'.format( str(i), x))
+        else:
+            goldFile.write('{0}\t{1}\n'.format( u'-', x))
     file.write('\n')
+    goldFile.write('\n')
 file.close()
+goldFile.close()
